d28f18c4a8c7d932cc80486788132b4fa0080ad8
tejpal-sohal <tejpal54@gmail.com>
2019-09-06 15:25:55 +0100

    new website 2019

---
+++ precache-manifest.59f1b9be20ed27c329718198323e0f88.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15d3b17388d53ba861893b0fd0d50ef3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cc03faf6cf36be6f922",
+    "url": "/static/css/main.c034a00e.chunk.css"
+  },
+  {
+    "revision": "2298325ee51f01883999",
+    "url": "/static/js/2.3720c36b.chunk.js"
+  },
+  {
+    "revision": "9cc03faf6cf36be6f922",
+    "url": "/static/js/main.0215074a.chunk.js"
+  },
+  {
+    "revision": "8feeb9981b0468271d97",
+    "url": "/static/js/runtime~main.fe134c89.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
